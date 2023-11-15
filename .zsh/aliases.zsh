c8d31865158b25c0c805c0764cc6157f20e384d6
Diego Porto <diego.cfporto@gmail.com>
2023-11-15 08:04:47 -0300

    update aliases and sdks

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,16 +1,20 @@
-# mac network
-alias ip="ipconfig getifaddr en0"
-
 # config files
 alias sshconfig="vim ~/.ssh/config"
 alias zshconfig="vim ~/.zshrc"
-
-# refresh zsh
 alias zshsource="source ~/.zshrc"
+
+# networking
+alias ip="ipconfig getifaddr en0"
+alias wanip="curl -4s https://ident.me/ip"
+alias wanip6="curl -6s https://ident.me/ip"
 
 # user commands
 alias grep='grep --color'
 alias ls="exa --icons --group-directories-first -F -H"
-alias ll="exa --icons --group-directories-first --long --header -alF"
+alias ll="exa --icons --group-directories-first --long --header -lF"
 alias tree="exa --icons --group-directories-first --long --header -T"
 
+# flecto-aliases
+alias flecto-local="docker exec -it backend-backend-1 bash"
+alias flecto-prod="heroku run bash -a rnters-prod"
+alias flecto-staging="heroku run bash -a rnters-staging"
---
+++ .zsh/sdk.zsh
@@ -0,0 +1,4 @@
+
+# android sdk
+ANDROID_SDK=~/Library/Android/sdk
+export PATH="$PATH:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools"
