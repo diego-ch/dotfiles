2e6f9cbd455b271d497f8fbb32cfeb65cafecf07
Diego Porto <diego.cfporto@gmail.com>
2026-02-03 10:18:41 -0300

    zsh: update aliases and sdk

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,31 +1,40 @@
 # config files
-alias sshconfig="vim ~/.ssh/config"
-alias zshconfig="vim ~/.zshrc"
+alias ghosttycfg="nvim ~/.config/ghostty/config"
+alias sshconfig="nvim ~/.ssh/config"
+alias zshconfig="nvim ~/.zshrc"
+alias zshaliases="nvim ~/.zsh/aliases.zsh"
 alias zshsource="source ~/.zshrc"
+
+# kitty
+alias icat="kitty +kitten icat"
+alias kssh="kitty +kitten ssh"
+alias ktheme="kitty +kitten themes"
 
 # networking
 alias lanip="ifconfig en0 | sort | grep inet | grep -v deprecated"
 alias wanip="curl -4s https://ident.me/ip && echo"
 alias wanip6="curl -6s https://ident.me/ip && echo"
+alias speedtest-best="speedtest -s 46129"
 
 # user commands
 alias grep='grep --color'
-alias ls="lsd"
-alias la="ls -a"
+alias ls="eza --group-directories-first --icons --time-style long-iso"
+alias la="ls -A"
 alias ll="ls -l"
-alias lla="ls -la"
+alias lla="ls -lA"
 alias lt="ls --tree"
-alias lta="lt -aI .git"
+alias lta="lt -AI .git"
 
 # flecto-aliases
+alias flecto-dev="wd flecto-be; docker compose run --rm console-test"
 alias flecto-local="docker exec -it backend-backend-1 bash"
 alias flecto-prod="heroku run bash -a rnters-prod"
 alias flecto-staging="heroku run bash -a rnters-staging"
 
 # docker aliases
-alias dcctnrm='for docker_container in $(docker ps -aq); do docker rm -f $docker_container; done;'
-alias dcimgrm='for docker_image in $(docker images -aq); do docker rmi -f $docker_image; done;'
-alias dcvolrm='for docker_volume in $(docker volume ls -q); do docker volume rm $docker_volume; done;'
+alias dcctnrm='for docker_ctn in $(docker ps -aq); do docker rm -f $docker_ctn; done;'
+alias dcimgrm='for docker_img in $(docker images -aq); do docker rmi -f $docker_img; done;'
+alias dcvolrm='for docker_vol in $(docker volume ls -q); do docker volume rm $docker_vol; done;'
 
 dockerpurge() {
     # show pre stats
@@ -33,7 +42,7 @@ dockerpurge() {
     echo
 
     # purge docker
-    dcctrm
+    dcctnrm
     dcimgrm
     dcvolrm
     docker network prune -f
@@ -43,3 +52,4 @@ dockerpurge() {
     echo
     docker system df
 }
+
--- .zsh/sdk.zsh
+++ .zsh/sdk.zsh
@@ -4,4 +4,4 @@ ANDROID_SDK=~/Library/Android/sdk
 export PATH="$PATH:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools"
 
 # flutter sdk
-export FLUTTER_ROOT="$(asdf where flutter)"
+export FLUTTER_ROOT="$(mise where flutter)"
