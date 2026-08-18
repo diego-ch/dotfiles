d880d4ef2b88bc484b6a988ab0fe9aefa1200149
Diego Porto <diego.cfporto@gmail.com>
2026-08-18 11:48:59 -0300

    zsh: update aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,44 +1,64 @@
+#
 # config files
+#
+
 alias ghosttycfg="nvim ~/.config/ghostty/config"
 alias kittycfg="nvim ~/.config/kitty/kitty.conf"
 alias sshconfig="nvim ~/.ssh/config"
 alias tmuxconfig="nvim ~/.config/tmux/tmux.conf"
 alias zshconfig="nvim ~/.zshrc"
 alias zshaliases="nvim ~/.zsh/aliases.zsh"
+
+#
+# reload ZSH
+#
+
 alias zshsource="source ~/.zshrc"
 
+#
 # kitty
+#
+
 alias icat="kitty +kitten icat"
 alias kssh="kitty +kitten ssh"
 alias ktheme="kitty +kitten themes"
 
+#
 # networking
+#
+
 alias lanip="ifconfig en0 | sort | grep inet | grep -v deprecated"
 alias wanip="curl -4s https://ident.me/ip && echo"
 alias wanip6="curl -6s https://ident.me/ip && echo"
 alias speedtest-best="speedtest -s 46129"
 
-# user commands
-alias grep='grep --color'
-alias ls="eza --group-directories-first --icons --time-style long-iso"
+#
+# override ls
+#
+
+export EZA_OPTIONS="--group-directories-first --icons --time-style long-iso"
+alias ls="eza ${EZA_OPTIONS}"
 alias la="ls -A"
 alias ll="ls -l"
 alias lla="ls -lA"
-alias lt="ls --tree"
+alias lt="ls -T"
 alias lta="lt -AI .git"
-alias lg="lazygit"
 
-# flecto-aliases
-alias flecto-dev="wd flecto-be; docker compose run --rm console-test"
-alias flecto-local="docker exec -it backend-backend-1 bash"
-alias flecto-prod="heroku run bash -a rnters-prod"
-alias flecto-staging="heroku run bash -a rnters-staging"
+#
+# override grep
+#
 
+alias grep='rg'
+
+#
 # docker aliases
+#
+
 alias dctstopall="docker stop $(docker ps -q)"
 alias dcctnrm='for docker_ctn in $(docker ps -aq); do docker rm -f $docker_ctn; done;'
 alias dcimgrm='for docker_img in $(docker images -aq); do docker rmi -f $docker_img; done;'
 alias dcvolrm='for docker_vol in $(docker volume ls -q); do docker volume rm $docker_vol; done;'
+alias dcps='docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Image}}\t{{.Command}}"'
 
 dockerpurge() {
     # show pre stats
@@ -56,4 +76,3 @@ dockerpurge() {
     echo
     docker system df
 }
-
